efbd6d5d0aac700182c07f6939e360abf4c0787d
qwertyhoge <cood.gh47@hotmail.co.jp>
2018-03-08 16:13:17 +0900

    Made simple list stack without deleting

--- TaskList/Form1.Designer.cs
+++ TaskList/Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.taskTablePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.headerLabelTime = new System.Windows.Forms.Label();
+            this.headerLabelEditor = new System.Windows.Forms.Label();
+            this.headerLabelDestroyer = new System.Windows.Forms.Label();
+            this.headerLabelName = new System.Windows.Forms.Label();
+            this.taskGeneratorButton = new System.Windows.Forms.Button();
+            this.taskTablePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(489, 23);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 19);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(489, 66);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(100, 19);
+            this.timeTextBox.TabIndex = 3;
+            // 
+            // taskTablePanel
+            // 
+            this.taskTablePanel.AutoScroll = true;
+            this.taskTablePanel.AutoSize = true;
+            this.taskTablePanel.ColumnCount = 4;
+            this.taskTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.09119F));
+            this.taskTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.09119F));
+            this.taskTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.90881F));
+            this.taskTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.90881F));
+            this.taskTablePanel.Controls.Add(this.headerLabelTime, 1, 0);
+            this.taskTablePanel.Controls.Add(this.headerLabelEditor, 2, 0);
+            this.taskTablePanel.Controls.Add(this.headerLabelDestroyer, 3, 0);
+            this.taskTablePanel.Controls.Add(this.headerLabelName, 0, 0);
+            this.taskTablePanel.Location = new System.Drawing.Point(24, 23);
+            this.taskTablePanel.Name = "taskTablePanel";
+            this.taskTablePanel.RowCount = 1;
+            this.taskTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.taskTablePanel.Size = new System.Drawing.Size(380, 90);
+            this.taskTablePanel.TabIndex = 4;
+            // 
+            // headerLabelTime
+            // 
+            this.headerLabelTime.AutoSize = true;
+            this.headerLabelTime.Location = new System.Drawing.Point(128, 0);
+            this.headerLabelTime.Name = "headerLabelTime";
+            this.headerLabelTime.Size = new System.Drawing.Size(29, 12);
+            this.headerLabelTime.TabIndex = 5;
+            this.headerLabelTime.Text = "期限";
+            // 
+            // headerLabelEditor
+            // 
+            this.headerLabelEditor.AutoSize = true;
+            this.headerLabelEditor.Location = new System.Drawing.Point(253, 0);
+            this.headerLabelEditor.Name = "headerLabelEditor";
+            this.headerLabelEditor.Size = new System.Drawing.Size(56, 12);
+            this.headerLabelEditor.TabIndex = 6;
+            this.headerLabelEditor.Text = "編集ボタン";
+            // 
+            // headerLabelDestroyer
+            // 
+            this.headerLabelDestroyer.AutoSize = true;
+            this.headerLabelDestroyer.Location = new System.Drawing.Point(317, 0);
+            this.headerLabelDestroyer.Name = "headerLabelDestroyer";
+            this.headerLabelDestroyer.Size = new System.Drawing.Size(56, 12);
+            this.headerLabelDestroyer.TabIndex = 7;
+            this.headerLabelDestroyer.Text = "削除ボタン";
+            // 
+            // headerLabelName
+            // 
+            this.headerLabelName.AutoSize = true;
+            this.headerLabelName.Location = new System.Drawing.Point(3, 0);
+            this.headerLabelName.Name = "headerLabelName";
+            this.headerLabelName.Size = new System.Drawing.Size(29, 12);
+            this.headerLabelName.TabIndex = 0;
+            this.headerLabelName.Text = "名前";
+            // 
+            // taskGeneratorButton
+            // 
+            this.taskGeneratorButton.Location = new System.Drawing.Point(489, 132);
+            this.taskGeneratorButton.Name = "taskGeneratorButton";
+            this.taskGeneratorButton.Size = new System.Drawing.Size(100, 40);
+            this.taskGeneratorButton.TabIndex = 5;
+            this.taskGeneratorButton.Text = "Generate Task";
+            this.taskGeneratorButton.UseVisualStyleBackColor = true;
+            this.taskGeneratorButton.Click += new System.EventHandler(this.taskGeneratorButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 370);
+            this.Controls.Add(this.taskGeneratorButton);
+            this.Controls.Add(this.taskTablePanel);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.taskTablePanel.ResumeLayout(false);
+            this.taskTablePanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.TableLayoutPanel taskTablePanel;
+        private System.Windows.Forms.Label headerLabelTime;
+        private System.Windows.Forms.Label headerLabelName;
+        private System.Windows.Forms.Label headerLabelEditor;
+        private System.Windows.Forms.Label headerLabelDestroyer;
+        private System.Windows.Forms.Button taskGeneratorButton;
     }
 }
 
--- TaskList/Form1.cs
+++ TaskList/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,74 @@ namespace TaskList
 {
     public partial class Form1 : Form
     {
+        int row = 1;
+        
         public Form1()
         {
             InitializeComponent();
         }
+        
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void taskGeneratorButton_Click(object sender, EventArgs e)
+        {
+            TextBox taskName = new TextBox();
+            TextBox taskTime = new TextBox();
+            Button editor = new Button();
+            
+            taskName.Name = "taskName" + row;
+            taskName.Text = nameTextBox.Text;
+            taskName.Size = new Size(100, 50);
+            taskName.ReadOnly = true;
+            taskName.Multiline = true;
+            taskTime.Name = "taskTime" + row;
+            taskTime.Text = timeTextBox.Text;
+            taskTime.Size = new Size(100, 50);
+            taskTime.ReadOnly = true;
+            taskTime.Multiline = true;
+            editor.Click += Editor_Click;
+            editor.Name = "editor" + row;
+            editor.Text = "編集";
+            editor.Size = new Size(70,20);
+
+
+            taskTablePanel.Controls.Add(taskName , 0, row);
+            taskTablePanel.Controls.Add(taskTime, 1, row);
+            taskTablePanel.Controls.Add(editor, 2, row);
+            row = row + 1;
+        }
+
+        private void Editor_Click(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+            int rowNum = Int32.Parse(Regex.Match(senderButton.Name, @"\d+").Value);
+            TextBox taskName = (TextBox)taskTablePanel.GetControlFromPosition(0, rowNum);
+            TextBox taskTime = (TextBox)taskTablePanel.GetControlFromPosition(1, rowNum);
+
+
+            if (senderButton.Text == "編集")
+            {
+                taskName.ReadOnly = false;
+                taskTime.ReadOnly = false;
+                senderButton.Text = "完了";
+            }else if(senderButton.Text == "完了")
+            {
+                taskName.ReadOnly = true;
+                taskTime.ReadOnly = true;
+                senderButton.Text = "編集";
+            }
+        
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
     }
 }
